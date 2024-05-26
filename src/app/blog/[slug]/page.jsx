3df95453d989540434d4419dc55e359444cfe2b2
JonLeCruc <110398234+JonLeCruc@users.noreply.github.com>
2024-05-26 19:08:46 +0200

    Home progress

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,25 @@
 import styles from "./singePost.module.css"
 import Image from "next/image";
+import { Suspense } from "react";
+import PostUser from "@/components/postUser/postUser";
+import { getPost, getPosts } from "@/lib/data";
 
-const SingePostPage = () => {
+// const getData = async (slug) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+
+//   /*if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }*/
+
+//   return res.json();
+// };
+
+
+const SingePostPage = async ({ params }) => {
+  const { slug } = params;
+
+  // const post = await getData();
+  const post = await getPost(slug);
     return (
       <div className={styles.container}>
         <div className={styles.imgContainer}>
@@ -13,7 +31,7 @@ const SingePostPage = () => {
            />
         </div>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>Title</h1>
+          <h1 className={styles.title}>{post.title}</h1>
           <div className={styles.detail}>
             <Image className={styles.avatar}
             src="https://images.pexels.com/photos/23549210/pexels-photo-23549210/free-photo-of-lys-hav-sort-hvid-landskab.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
@@ -21,17 +39,16 @@ const SingePostPage = () => {
             width={50}
             height={50}
              />
-             <div className={styles.detailText}>
-              <span className={styles.detailTitle}>Author</span>
-              <span className={styles.detailValue}>Jon Holm</span>
-             </div>
+             {post && <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId = {post.userId}/>
+             </Suspense>}
              <div className={styles.detailText}>
               <span className={styles.detailTitle}>Published</span>
               <span className={styles.detailValue}>01.01.2024</span>
              </div>
           </div>
           <div className={styles.content}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis adipisci minima reiciendis a autem assumenda dolore.
+            {post.body}
           </div>
         </div>
       </div>
